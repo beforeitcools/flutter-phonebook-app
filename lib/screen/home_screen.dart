a6dcfc4950194b595ae4e9460691d6feb723fe42
yzzzang <yz1211ch@gmail.com>
2024-12-18 16:57:18 +0900

    feat: set main page

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -15,15 +15,6 @@ class _HomeScreenState extends State<HomeScreen> {
         title: Text("Flutter Phone Book"),
         backgroundColor: Colors.black,
       ),
-      bottomNavigationBar: BottomNavigationBar(
-          type: BottomNavigationBarType.fixed,
-          items: const[
-            BottomNavigationBarItem(icon: Icon(Icons.home), label: "홈"),
-            BottomNavigationBarItem(icon: Icon(Icons.search), label: "번호검색"),
-            BottomNavigationBarItem(icon: Icon(Icons.add), label: "번호등록"),
-            BottomNavigationBarItem(icon: Icon(Icons.edit), label: "번호수정"),
-            BottomNavigationBarItem(icon: Icon(Icons.delete), label: "번호삭제"),
-          ]),
     );
   }
 }
